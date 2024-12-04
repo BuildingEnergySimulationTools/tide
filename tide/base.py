556034b9824f108ca007c5c485ec887d12371c36
BaptisteDE <bdurandestebe@nobatek.inef4.com>
2024-12-04 17:40:52 +0100

    ✨ BaseFiller base class

--- tide/base.py
+++ tide/base.py
@@ -2,7 +2,9 @@ import datetime as dt
 import typing
 from abc import ABC, abstractmethod
 
+import numpy as np
 import pandas as pd
+
 from sklearn.base import TransformerMixin, BaseEstimator
 from sklearn.utils import check_array
 from sklearn.utils.validation import check_is_fitted
@@ -12,6 +14,7 @@ from tide.utils import (
     timedelta_to_int,
     validate_odd_param,
     process_stl_odd_args,
+    get_data_blocks,
 )
 
 
@@ -57,3 +60,38 @@ class BaseSTL(ABC, BaseEstimator):
         if self.seasonal is not None:
             self.stl_kwargs["seasonal"] = self.seasonal
             process_stl_odd_args("seasonal", X, self.stl_kwargs)
+
+
+class BaseFiller:
+    def __init__(
+        self,
+        gaps_lte: str | pd.Timedelta | dt.timedelta = None,
+        gaps_gte: str | pd.Timedelta | dt.timedelta = None,
+    ):
+        self.gaps_lte = gaps_lte
+        self.gaps_gte = gaps_gte
+
+    def get_gaps_dict_to_fill(self, X: pd.Series | pd.DataFrame):
+        X = check_and_return_dt_index_df(X)
+        return get_data_blocks(
+            X,
+            is_null=True,
+            select_inner=False,
+            lower_td_threshold=self.gaps_lte,
+            upper_td_threshold=self.gaps_gte,
+            upper_threshold_inclusive=True,
+            lower_threshold_inclusive=True,
+            return_combination=False,
+        )
+
+    def get_gaps_mask(self, X: pd.Series | pd.DataFrame):
+        gaps_dict = self.get_gaps_dict_to_fill(X)
+        df_mask = pd.DataFrame(index=X.index)
+        for col, idx_list in gaps_dict.items():
+            if idx_list:
+                combined_idx = pd.concat([idx.to_series() for idx in idx_list]).index
+                df_mask[col] = X.index.isin(combined_idx)
+            else:
+                df_mask[col] = np.zeros_like(X.shape[0]).astype(bool)
+
+        return df_mask
